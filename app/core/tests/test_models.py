467e84185b01c468561c6525f38f77d5b8d9beff
Ernesto de Tovar <ernesto.detovar@enreda.coop>
2020-03-31 10:36:14 +0200

    Added working list tags endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from core import models
+
+
+def sample_user(email='test@gmail.com', password='test1234'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
+
     def test_create_user_with_email_successful(self):
         email = "test@email.com"
         password = "testpass123"
@@ -33,3 +41,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+    
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
 
+
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
@@ -47,7 +48,11 @@ class PublicUserApiTest(TestCase):
 
     def test_password_too_short(self):
         """Test that the password must be more than 5 characters"""
-        payload = {'email': 'test@gmail.com', 'password': 'pw', 'name': 'Test name'}
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'pw',
+            'name': 'Test name'
+        }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
